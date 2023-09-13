9ff46f1550a5efe6c46af16094e4120e3d53131f
MariusTulbure <marius.tulbure00@gmail.com>
2023-09-13 12:12:08 +0300

    add destroy method and actions on articles pages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -34,4 +34,10 @@ class ArticlesController < ApplicationController
       render :new, status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
 end
