76bb385e3fd7a888a2bf9fdf47156e70c46067cf
Marius Tulbure <marius.tulbure00@gmail.com>
2023-09-18 13:59:24 +0300

    add login and logout buttons

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
+before_action :set_user, only: %i[show edit update]
+
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
-
   end
 
   def new
@@ -14,11 +14,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = 'Your account information was successfully updated'
       redirect_to @user
@@ -30,6 +28,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "#{@user.username.capitalize} welcome to the Alpha blog."
       redirect_to @user
     else
@@ -38,6 +37,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
